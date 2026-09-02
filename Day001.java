7a8ba01cd89cccf210b112a83f9b4c2a58428438
MuhammadThaufiq95 <santuybang819@gmail.com>
2026-09-02 19:29:43 +0700

    Update Day001.java
    Update:
    Added explanation about each parts of the program

--- Day001.java
+++ Day001.java
@@ -15,9 +15,26 @@ Biasanya digunakan untuk menjelaskan codingan
  * 
  */
 
+//Class
+/**
+* Class adalah struktur data dasar dalam Java yang digunakan
+* untuk mengelompokkan data dan method dalam satu kesatuan
+*/
+
+//nama class pada program ini adalah Day001
 public class Day001 {
+    
+/**
+* MAIN METHOD
+* Method main merupakan method utama dalam program java
+* Ketika program dijalankan, Java akan mulai eksekusi
+* dari method main ini
+*/
     public static void main(String[] args) {
-        
+        // public: method dapat diakses oleh Java Runtime Environment
+        // static: method dapat dijalankan tanpa membuat objek dari class
+        // void: method tidak mengembalikan nilai
+        // (String[] args): parameter untuk menerima argumen dari command line
     }
     
 }
