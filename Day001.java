e0f1fbdfa6ab79bbd7d8addcdceadfaeb40b59ea
MuhammadThaufiq95 <santuybang819@gmail.com>
2026-09-02 19:05:16 +0700

    Add files via upload
    Day 1:
    Explaining about every type of comments and understand the basic program structure in Java

---
+++ Day001.java
@@ -0,0 +1,23 @@
+package pkg100daysofngoding;
+
+// Example 1: this is a single line comment
+
+/* Example 2:
+this is a multiline comment
+Biasanya digunakan untuk menjelaskan codingan
+*/
+
+/**
+ * Example 3: This is a Docummentation Comment
+ * Biasanya untuk mendokumentasikan codingan/program
+ * 
+ * @author ASUS
+ * 
+ */
+
+public class Day001 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
