2529d1ad0e3f072076342a7de5308188dc4e39b4
MuhammadThaufiq95 <santuybang819@gmail.com>
2026-09-07 08:12:33 +0700

    Add files via upload
    Day 6
    Using character data type and boolean data type such as char and boolean

---
+++ Day006.java
@@ -0,0 +1,26 @@
+/*
+ * Day 6: Using Character data type, such as char and boolean
+ */
+package pkg100daysofngoding;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day006 {
+    public static void main(String[] args) {
+        char huruf = 'A';
+        char simbol = '$';
+        char unicode = '\u0043'; // unicode untuk menampilkan huruf C.
+        
+        boolean yes = true;
+        boolean no = false;
+        
+        System.out.println(huruf);
+        System.out.println(simbol);
+        System.out.println(unicode);
+        
+        System.out.println(yes);
+        System.out.println(no);
+    }
+}
