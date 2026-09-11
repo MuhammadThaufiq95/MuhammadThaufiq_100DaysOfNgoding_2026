0741265bfc73946448a8270f07191f549bfda126
MuhammadThaufiq95 <santuybang819@gmail.com>
2026-09-11 17:00:11 +0700

    Add files via upload
    Day 10:
    learning about minimum limit and maximum limit of a data type

---
+++ Day010.java
@@ -0,0 +1,32 @@
+/*
+ * Day 10: Memahami batas minimal dan batas maksimal
+ * dari tipe data
+ */
+package pkg100daysofngoding;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day010 {
+    public static void main(String[] args) {
+        
+        //batas minimal dan maksimal byte.
+        System.out.println("byte  : " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+        
+        //batas minimal dan maksimal short.
+        System.out.println("short : " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+        
+        //batas minimal dan maksimal int.
+        System.out.println("int   : " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+        
+        //batas minimal dan maksimal long
+        System.out.println("long  : " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+        
+        //batas minimal dan maksimal float.
+        System.out.println("float : " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+        
+        //batas minimal dan maksimal double
+        System.out.println("double: " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+    }
+}
