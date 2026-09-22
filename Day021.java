2767d316b01122655c27813cc835d7b625ede886
MuhammadThaufiq95 <santuybang819@gmail.com>
2026-09-22 19:17:11 +0700

    Add files via upload
    Day 21: Learning how to convert String into primitive data type

---
+++ Day021.java
@@ -0,0 +1,36 @@
+/*
+ * Day 21: Converting string into primitive data type
+ */
+package pkg100daysofngoding;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your age   : ");
+        String number = sc.nextLine();
+        int angka = Integer.parseInt(number);
+        
+        System.out.print("Enter your height: ");
+        String decimal = sc.nextLine();
+        double desimal = Double.parseDouble(decimal);
+        
+        double hasil = desimal / 100;
+        
+        System.out.print("status as student: ");
+        String statusBefore = sc.nextLine();
+        boolean statusAfter = Boolean.parseBoolean(statusBefore);
+        
+        System.out.println("\n\n\n--------Result--------");
+        System.out.println("Age           : "+angka);
+        System.out.println("Height        : "+hasil+"m");
+        System.out.println("Student Status: "+statusAfter);
+        
+    }
+}
